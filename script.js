54fb4500d580b46bddde66a3649b7d6b5d213482
Ashar Khan <asharullah.khan2468@gmail.com>
2024-05-01 19:56:21 +0500

    DOM Parctice

---
+++ script.js
@@ -0,0 +1,26 @@
+// let value= document.createElement("p");
+
+// let app = value.innerHTML="Hemlo jee !";
+
+// document.querySelector(".para").appendChild(value);
+
+
+// let container = document.querySelector(".parent");
+
+
+// console.log(container.childNodes[2]);
+
+
+// let para = document.querySelector(".para");
+
+// para.Style.BackgroundColor="green"
+
+
+// console.log(para.innerHTML);
+
+const para = document.querySelectorAll("h3");
+
+console.log(para[3]);
+
+
+
