9633a0a456ca40975b319150b827ac508980bfd5
Ashar Khan <asharullah.khan2468@gmail.com>
2024-05-02 14:45:06 +0500

    DOM Practice

--- script.js
+++ script.js
@@ -18,9 +18,48 @@
 
 // console.log(para.innerHTML);
 
-const para = document.querySelectorAll("h3");
+// const para = document.querySelectorAll("h3");
 
-console.log(para[3]);
+// console.log(para[3]);
+
+
+// const ul = document.querySelector("ul");
+
+// console.log(ul[2]);
+
+// const myUl = document.querySelectorAll("li");
+// console.log(myUl);
+// Array.from(myUl);
+// myUl.style.backgroundColor="NavyBlue"
+
+// console.log(myUl[1].style.backgroundColor="yellow");
+// console.log(myUl[2].style.backgroundColor="blue");
+// console.log(myUl[3].style.backgroundColor="green");
+
+
+
+
+
+const parent = document.querySelector(".parent");
+
+// console.log(parent);
+// console.log(parent.children[4].innerHTML);
+
+
+
+// for (let i = 0 ; i < parent.children.length;  i++){
+
+//     console.log(parent.children[i].innerHTML);
+
+// }
+
+// parent.children[1].style.backgroundColor="green";
+
+
+// console.log(parent.lastElementChild);
+
+
+// console.log(parent.childNodes);
 
 
 
